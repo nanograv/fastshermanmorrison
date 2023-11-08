592567d24dfbe51283db23a91387b1259c7518ae
Rutger van Haasteren <vhaasteren@gmail.com>
2023-11-08 17:51:14 +0100

    Updated the versioning

--- fastshermanmorrison/__init__.py
+++ fastshermanmorrison/__init__.py
@@ -1,2 +1,6 @@
 from . import fastshermanmorrison
-from .version import __version__
+try:
+    from ._version import __version__
+except ImportError:
+    # _version.py will be missing during package setup when setuptools_scm has not yet written it
+    __version__ = "unknown"
